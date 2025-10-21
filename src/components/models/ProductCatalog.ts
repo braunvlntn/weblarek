6643d429c10842e613c50afa04d77dfbc37f7aa6
Valentin Braun <braunvlntn@yandex.ru>
2025-10-21 21:41:35 +0500

    Реализовал класс каталога товаров

---
+++ src/components/models/ProductCatalog.ts
@@ -0,0 +1,32 @@
+import { IProduct } from "../../types";
+
+type TSelectedProduct = IProduct | null;
+
+export class ProductCatalog {
+  products: IProduct[] = [];
+  selectedProduct: TSelectedProduct = null;
+
+  constructor(products: IProduct[]) {
+    this.setProducts(products);
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  setProducts(products: IProduct[]) {
+    this.products = products;
+  }
+
+  getProductById(productId: IProduct["id"]) {
+    return this.products.find((product) => product.id === productId);
+  }
+
+  getSelectedProduct() {
+    return this.selectedProduct;
+  }
+
+  setSelectedProduct(product: TSelectedProduct) {
+    this.selectedProduct = product;
+  }
+}
