ef1f7eab5a2bfcaa5ed1a282d972be243aba07b4
Valentin Braun <braunvlntn@yandex.ru>
2025-10-23 19:40:02 +0500

    Реализована проверка получения и сохранения товаров

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-import './scss/styles.scss';
+import "./scss/styles.scss";
+import { ProductCatalog } from "./components/models/ProductCatalog.ts";
+import { DataFetcher } from "./components/communication/DataFetcher.ts";
+import { Api } from "./components/base/Api.ts";
+import { API_URL } from "./utils/constants.ts";
+
+const dataFetcher = new DataFetcher(new Api(API_URL));
+
+const fetchedProducts = await dataFetcher.fetchProducts();
+
+const productCatalog = new ProductCatalog(fetchedProducts.items);
+
+console.log(productCatalog.getProducts());
