e0980f2fc564a78ce6839e4515f64eb447f19459
Valentin Braun <braunvlntn@yandex.ru>
2025-10-28 18:09:35 +0500

    Добавил обработку ошибки при загружке товаров

--- src/main.ts
+++ src/main.ts
@@ -5,97 +5,110 @@ import { Api } from "./components/base/Api.ts";
 import { API_URL } from "./utils/constants.ts";
 import { Cart } from "./components/models/Cart.ts";
 import { Buyer } from "./components/models/Buyer.ts";
+import { IProduct } from "./types";
 
 const dataFetcher = new DataFetcher(new Api(API_URL));
 
-const fetchedProducts = await dataFetcher.fetchProducts();
+const loadProducts = async (): Promise<IProduct[] | null> => {
+  try {
+    const fetchedProducts = await dataFetcher.fetchProducts();
 
-const productCatalog = new ProductCatalog();
+    return fetchedProducts.items;
+  } catch (error) {
+    console.error("Ошибка загрузки товаров: ", error);
 
-productCatalog.setProducts(fetchedProducts.items);
+    return null;
+  }
+};
 
-console.log("Товары", productCatalog.getProducts());
+const products = await loadProducts();
 
-console.log(
-  "Получение товаров по id",
-  productCatalog.getProductById(fetchedProducts.items[1].id)
-);
+if (products) {
+  console.log("Товары загружены");
 
-productCatalog.setSelectedProduct(fetchedProducts.items[1]);
+  const productCatalog = new ProductCatalog();
 
-console.log("Выбранный товар", productCatalog.getSelectedProduct());
+  productCatalog.setProducts(products);
 
-const cart = new Cart();
+  console.log("Товары", productCatalog.getProducts());
 
-cart.addProduct(productCatalog.getProducts()[0]);
-cart.addProduct(productCatalog.getProducts()[1]);
+  console.log(
+    "Получение товаров по id",
+    productCatalog.getProductById(products[1].id)
+  );
 
-console.log("Товары в корзине", cart.getProducts());
-console.log(
-  "Проверка товара в корзине по id",
-  cart.checkProductExistence(productCatalog.getProducts()[1].id)
-);
-console.log("Стоимость всех товаров", cart.getAllProductsCost());
-console.log("Количество товаров", cart.getAllProductsNumber());
+  productCatalog.setSelectedProduct(products[1]);
 
-console.log("Удаление товара с индексом 1");
-cart.removeProduct(productCatalog.getProducts()[1].id);
+  console.log("Выбранный товар", productCatalog.getSelectedProduct());
 
-console.log("Товары в корзине", cart.getProducts());
-console.log(
-  "Проверка товара в корзине по id",
-  cart.checkProductExistence(productCatalog.getProducts()[1].id)
-);
-console.log("Стоимость всех товаров", cart.getAllProductsCost());
-console.log("Количество товаров", cart.getAllProductsNumber());
+  const cart = new Cart();
 
-console.log("Удаление всех товаров");
-cart.clearProducts();
+  cart.addProduct(productCatalog.getProducts()[0]);
+  cart.addProduct(productCatalog.getProducts()[1]);
 
-console.log("Товары в корзине", cart.getProducts());
-console.log(
-  "Проверка товара в корзине по id",
-  cart.checkProductExistence(productCatalog.getProducts()[0].id)
-);
-console.log("Стоимость всех товаров", cart.getAllProductsCost());
-console.log("Количество товаров", cart.getAllProductsNumber());
+  console.log("Товары в корзине", cart.getProducts());
+  console.log(
+    "Проверка товара в корзине по id",
+    cart.checkProductExistence(productCatalog.getProducts()[1].id)
+  );
+  console.log("Стоимость всех товаров", cart.getAllProductsCost());
+  console.log("Количество товаров", cart.getAllProductsNumber());
 
-const buyer = new Buyer();
+  console.log("Удаление товара с индексом 1");
+  cart.removeProduct(productCatalog.getProducts()[1].id);
 
-console.log("Валидация (все поля пустые)", buyer.validateBuyersData());
+  console.log("Товары в корзине", cart.getProducts());
+  console.log(
+    "Проверка товара в корзине по id",
+    cart.checkProductExistence(productCatalog.getProducts()[1].id)
+  );
+  console.log("Стоимость всех товаров", cart.getAllProductsCost());
+  console.log("Количество товаров", cart.getAllProductsNumber());
 
-buyer.setAddress("Не дом и не улица, Советский Союз");
+  console.log("Удаление всех товаров");
+  cart.clearProducts();
 
-console.log("Валидация (указан адрес)", buyer.validateBuyersData());
+  console.log("Товары в корзине", cart.getProducts());
+  console.log(
+    "Проверка товара в корзине по id",
+    cart.checkProductExistence(productCatalog.getProducts()[0].id)
+  );
+  console.log("Стоимость всех товаров", cart.getAllProductsCost());
+  console.log("Количество товаров", cart.getAllProductsNumber());
 
-buyer.setPhone("8-800-555-35-35");
+  const buyer = new Buyer();
 
-console.log("Валидация (указан телефон)", buyer.validateBuyersData());
+  console.log("Валидация (все поля пустые)", buyer.validateBuyersData());
 
-buyer.setPayment("card");
+  buyer.setAddress("Не дом и не улица, Советский Союз");
+  console.log("Валидация (указан адрес)", buyer.validateBuyersData());
 
-console.log(
-  "Валидация (указан указан способ оплаты)",
-  buyer.validateBuyersData()
-);
+  buyer.setPhone("8-800-555-35-35");
+  console.log("Валидация (указан телефон)", buyer.validateBuyersData());
 
-buyer.setEmail("top_frontend@yandex.ru");
+  buyer.setPayment("card");
+  console.log(
+    "Валидация (указан указан способ оплаты)",
+    buyer.validateBuyersData()
+  );
 
-console.log(
-  "Валидация (указан адрес электронной почты)",
-  buyer.validateBuyersData()
-);
+  buyer.setEmail("top_frontend@yandex.ru");
+  console.log(
+    "Валидация (указан адрес электронной почты)",
+    buyer.validateBuyersData()
+  );
 
-console.log("Электронная почта", buyer.getEmail());
-console.log("Адрес", buyer.getAddress());
-console.log("Способ оплаты", buyer.getPayment());
-console.log("Номер телефона", buyer.getPhone());
+  console.log("Электронная почта", buyer.getEmail());
+  console.log("Адрес", buyer.getAddress());
+  console.log("Способ оплаты", buyer.getPayment());
+  console.log("Номер телефона", buyer.getPhone());
 
-console.log("Очистка данных покупателя");
-buyer.clearBuyersData();
+  console.log("Очистка данных покупателя");
+  buyer.clearBuyersData();
 
-console.log("Электронная почта", buyer.getEmail());
-console.log("Адрес", buyer.getAddress());
-console.log("Способ оплаты", buyer.getPayment());
-console.log("Номер телефона", buyer.getPhone());
-console.log("Валидация после очистки", buyer.validateBuyersData());
+  console.log("Электронная почта", buyer.getEmail());
+  console.log("Адрес", buyer.getAddress());
+  console.log("Способ оплаты", buyer.getPayment());
+  console.log("Номер телефона", buyer.getPhone());
+  console.log("Валидация после очистки", buyer.validateBuyersData());
+}
