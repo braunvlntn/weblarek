92601197b8b1b43e1103f681257b4552ddf89f8a
Valentin Braun <braunvlntn@yandex.ru>
2025-11-08 00:09:46 +0500

    Реализовал представление корзины

---
+++ src/components/views/BasketView.ts
@@ -0,0 +1,46 @@
+import { ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component.ts";
+
+interface IBasket {
+  products: HTMLElement[];
+  basketPrice: number;
+}
+
+export class BasketView extends Component<IBasket> {
+  protected _basketList: HTMLElement;
+  protected _basketPrice: HTMLElement;
+  protected _basketButton: HTMLButtonElement;
+
+  constructor(container: HTMLElement, actions?: { onClick: () => void }) {
+    super(container);
+
+    this._basketList = ensureElement<HTMLElement>(".basket__list", container);
+
+    this._basketPrice = ensureElement<HTMLElement>(".basket__price", container);
+
+    this._basketButton = ensureElement<HTMLButtonElement>(
+      ".basket__button",
+      container
+    );
+
+    if (actions?.onClick) {
+      this._basketButton.addEventListener("click", actions.onClick);
+    }
+  }
+
+  set products(products: HTMLElement[]) {
+    if (!products.length) {
+      this._basketList.replaceChildren("Корзина пуста.");
+      this._basketButton.disabled = true;
+
+      return;
+    }
+
+    this._basketButton.disabled = false;
+    this._basketList.replaceChildren(...products);
+  }
+
+  set basketPrice(price: number) {
+    this._basketPrice.textContent = `${price} синапсов`;
+  }
+}
