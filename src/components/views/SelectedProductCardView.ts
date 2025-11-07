a67d93da6c7ddbe396babea06dfcbac3f1c0487d
Valentin Braun <braunvlntn@yandex.ru>
2025-11-07 15:19:04 +0500

    Реализовал класс отображения карточки товара в модальном окне

---
+++ src/components/views/SelectedProductCardView.ts
@@ -0,0 +1,48 @@
+import { ProductCardBaseView } from "./ProductCardBaseView";
+import { ensureElement } from "../../utils/utils";
+import { categoryMap } from "../../utils/constants.ts";
+import { IProduct } from "../../types";
+
+export class SelectedProductCardView extends ProductCardBaseView {
+  protected _image: HTMLImageElement;
+  protected _category: HTMLElement;
+  protected _description: HTMLElement;
+  protected _button: HTMLButtonElement;
+
+  constructor(container: HTMLElement, actions?: { onClick?: () => void }) {
+    super(container);
+
+    this._image = ensureElement<HTMLImageElement>(".card__image", container);
+    this._category = ensureElement<HTMLElement>(".card__category", container);
+    this._description = ensureElement<HTMLElement>(".card__text", container);
+    this._button = ensureElement<HTMLButtonElement>(".card__button", container);
+
+    if (this._button && actions?.onClick) {
+      this._button.addEventListener("click", actions.onClick);
+    }
+  }
+
+  set image(value: string) {
+    this.setImage(this._image, value, this.title);
+  }
+
+  set category(value: keyof typeof categoryMap) {
+    this._category.textContent = value;
+    this._category.className = `card__category ${this._getCategoryClass(
+      value
+    )}`;
+  }
+
+  set description(value: string) {
+    this._description.textContent = value;
+  }
+
+  set buttonText(value: string) {
+    this._button.textContent = value;
+  }
+
+  render(data?: Partial<IProduct & { buttonText: string }>): HTMLElement {
+    super.render(data);
+    return this.container;
+  }
+}
