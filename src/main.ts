783ffd4b25eec81530f2054270c142ff0d16d5b8
Valentin Braun <braunvlntn@yandex.ru>
2025-11-07 15:16:40 +0500

    Реализовал отображение каталога

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,56 @@
 import "./scss/styles.scss";
-import { ProductCatalog } from "./components/models/ProductCatalog.ts";
-import { DataFetcher } from "./components/communication/DataFetcher.ts";
-import { Api } from "./components/base/Api.ts";
-import { API_URL } from "./utils/constants.ts";
-import { Cart } from "./components/models/Cart.ts";
-import { Buyer } from "./components/models/Buyer.ts";
-import { IProduct } from "./types";
+import { API_URL, CDN_URL, Events } from "./utils/constants";
+import { EventEmitter } from "./components/base/Events";
+import { Api } from "./components/base/Api";
+import { ProductCatalog } from "./components/models/ProductCatalog";
+import { DataFetcher } from "./components/communication/DataFetcher";
+import { ProductCardCatalogView } from "./components/views/ProductCardCatalogView.ts";
+import { ProductCatalogView } from "./components/views/ProductCatalogView";
+import { cloneTemplate } from "./utils/utils";
 
-const dataFetcher = new DataFetcher(new Api(API_URL));
+// Инициализация EventEmitter для управления событиями
+const events = new EventEmitter();
 
-const loadProducts = async (): Promise<IProduct[] | null> => {
-  try {
-    const fetchedProducts = await dataFetcher.fetchProducts();
+// Инициализация API и DataFetcher
+const api = new Api(API_URL);
+const dataFetcher = new DataFetcher(api);
 
-    return fetchedProducts.items;
-  } catch (error) {
-    console.error("Ошибка загрузки товаров: ", error);
+// Инициализация моделей
+const productCatalog = new ProductCatalog();
 
-    return null;
-  }
-};
+// Инициализация представлений
+const catalogView = new ProductCatalogView(document.body);
 
-const products = await loadProducts();
+// Загрузка товаров при инициализации
+dataFetcher
+  .fetchProducts()
+  .then((products) => {
+    productCatalog.setProducts(products.items);
+    events.emit(Events.CATALOG_CHANGE);
+  })
+  .catch((error) => {
+    console.error("Ошибка загрузки товаров:", error);
+  });
 
-if (products) {
-  console.log("Товары загружены");
+// Обработчик изменения каталога
+events.on(Events.CATALOG_CHANGE, () => {
+  const products = productCatalog.getProducts();
 
-  const productCatalog = new ProductCatalog();
+  const productCards = products.map((product) => {
+    const cardElement = cloneTemplate<HTMLElement>("#card-catalog");
 
-  productCatalog.setProducts(products);
+    const card = new ProductCardCatalogView(cardElement, {
+      onClick: () => {},
+    });
 
-  console.log("Товары", productCatalog.getProducts());
+    return card.render({
+      id: product.id,
+      title: product.title,
+      image: CDN_URL + product.image,
+      category: product.category,
+      price: product.price,
+    });
+  });
 
-  console.log(
-    "Получение товаров по id",
-    productCatalog.getProductById(products[1].id)
-  );
-
-  productCatalog.setSelectedProduct(products[1]);
-
-  console.log("Выбранный товар", productCatalog.getSelectedProduct());
-
-  const cart = new Cart();
-
-  cart.addProduct(productCatalog.getProducts()[0]);
-  cart.addProduct(productCatalog.getProducts()[1]);
-
-  console.log("Товары в корзине", cart.getProducts());
-  console.log(
-    "Проверка товара в корзине по id",
-    cart.checkProductExistence(productCatalog.getProducts()[1].id)
-  );
-  console.log("Стоимость всех товаров", cart.getAllProductsCost());
-  console.log("Количество товаров", cart.getAllProductsNumber());
-
-  console.log("Удаление товара с индексом 1");
-  cart.removeProduct(productCatalog.getProducts()[1].id);
-
-  console.log("Товары в корзине", cart.getProducts());
-  console.log(
-    "Проверка товара в корзине по id",
-    cart.checkProductExistence(productCatalog.getProducts()[1].id)
-  );
-  console.log("Стоимость всех товаров", cart.getAllProductsCost());
-  console.log("Количество товаров", cart.getAllProductsNumber());
-
-  console.log("Удаление всех товаров");
-  cart.clearProducts();
-
-  console.log("Товары в корзине", cart.getProducts());
-  console.log(
-    "Проверка товара в корзине по id",
-    cart.checkProductExistence(productCatalog.getProducts()[0].id)
-  );
-  console.log("Стоимость всех товаров", cart.getAllProductsCost());
-  console.log("Количество товаров", cart.getAllProductsNumber());
-
-  const buyer = new Buyer();
-
-  console.log("Валидация (все поля пустые)", buyer.validateBuyersData());
-
-  buyer.setAddress("Не дом и не улица, Советский Союз");
-  console.log("Валидация (указан адрес)", buyer.validateBuyersData());
-
-  buyer.setPhone("8-800-555-35-35");
-  console.log("Валидация (указан телефон)", buyer.validateBuyersData());
-
-  buyer.setPayment("card");
-  console.log(
-    "Валидация (указан указан способ оплаты)",
-    buyer.validateBuyersData()
-  );
-
-  buyer.setEmail("top_frontend@yandex.ru");
-  console.log(
-    "Валидация (указан адрес электронной почты)",
-    buyer.validateBuyersData()
-  );
-
-  console.log("Электронная почта", buyer.getEmail());
-  console.log("Адрес", buyer.getAddress());
-  console.log("Способ оплаты", buyer.getPayment());
-  console.log("Номер телефона", buyer.getPhone());
-
-  console.log("Очистка данных покупателя");
-  buyer.clearBuyersData();
-
-  console.log("Электронная почта", buyer.getEmail());
-  console.log("Адрес", buyer.getAddress());
-  console.log("Способ оплаты", buyer.getPayment());
-  console.log("Номер телефона", buyer.getPhone());
-  console.log("Валидация после очистки", buyer.validateBuyersData());
-}
+  catalogView.render({ items: productCards });
+});
