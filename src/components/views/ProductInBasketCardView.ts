df807acd2efe87d849f38b79052031b7faaa25a2
Valentin Braun <braunvlntn@yandex.ru>
2025-11-08 00:12:01 +0500

    Реализовал карточку товара в корзине

---
+++ src/components/views/ProductInBasketCardView.ts
@@ -0,0 +1,33 @@
+import { ProductCardBaseView } from "./ProductCardBaseView.ts";
+import { ensureElement } from "../../utils/utils.ts";
+import { IProduct } from "../../types";
+
+interface IProductInBasketCardActions {
+  onClick: (event: MouseEvent) => void;
+}
+
+export class ProductInBasketCardView extends ProductCardBaseView {
+  protected _index: HTMLElement;
+  protected _button: HTMLButtonElement;
+
+  constructor(container: HTMLElement, actions?: IProductInBasketCardActions) {
+    super(container);
+
+    this._index = ensureElement<HTMLElement>(".basket__item-index", container);
+    this._button = ensureElement<HTMLButtonElement>(".card__button", container);
+
+    if (actions?.onClick) {
+      console.log("click");
+      this._button.addEventListener("click", actions.onClick);
+    }
+  }
+
+  set index(value: string) {
+    this._index.textContent = value;
+  }
+
+  render(data?: Partial<IProduct & { index: number }>): HTMLElement {
+    super.render(data);
+    return this.container;
+  }
+}
